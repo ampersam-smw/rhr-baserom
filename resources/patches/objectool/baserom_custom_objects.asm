4c9c07adf44538f2ac2f6a5f8c639229e370405c
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2024-07-16 20:17:00 -0230

    split out baserom reserved objects from main objectcode

---
+++ resources/patches/objectool/baserom_custom_objects.asm
@@ -0,0 +1,79 @@
+
+incsrc "../../../shared/freeram.asm"
+
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+; code for extended objects 98-CF
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+CustExObj98:
+CustExObj99:
+CustExObj9A:
+CustExObj9B:
+CustExObj9C:
+CustExObj9D:
+CustExObj9E:
+CustExObj9F:
+CustExObjA0:
+CustExObjA1:
+CustExObjA2:
+CustExObjA3:
+CustExObjA4:
+CustExObjA5:
+CustExObjA6:
+CustExObjA7:
+CustExObjA8:
+CustExObjA9:
+CustExObjAA:
+CustExObjAB:
+CustExObjAC:
+CustExObjAD:
+CustExObjAE:
+CustExObjAF:
+CustExObjB0:
+CustExObjB1:
+CustExObjB2:
+CustExObjB3:
+CustExObjB4:
+CustExObjB5:
+CustExObjB6:
+CustExObjB7:
+CustExObjB8:
+CustExObjB9:
+CustExObjBA:
+CustExObjBB:
+CustExObjBC:
+CustExObjBD:
+CustExObjBE:
+CustExObjBF:
+CustExObjC0:
+CustExObjC1:
+CustExObjC2:
+CustExObjC3:
+CustExObjC4:
+CustExObjC5:
+CustExObjC6:
+CustExObjC7:
+CustExObjC8:
+CustExObjC9:
+CustExObjCA:
+CustExObjCB:
+CustExObjCC:
+CustExObjCD:
+CustExObjCE:
+CustExObjCF:
+.set_object_flag
+    TXA				; get 2 * object number from X
+    LSR  			; divide by 2 since X was a word index
+	TAY  			; cache in Y
+	LSR #3			; divide by 8 since we have 8 bits/flags per byte
+	TAX				; use as index into the FreeRAM later
+	TYA				; restore object number from Y
+	AND #$07		; modulo 8 since we have 8 bits/flags per byte
+	TAY				; use result as loop variable
+	LDA ..masks,y	; load correct bit mask for corresponding bit
+    ORA !objectool_level_flags_bank,x   ; account for previously set flags
+    STA !objectool_level_flags_bank,x   ; store flag byte
+    RTS
+
+..masks
+	db 1,2,4,8,16,32,64,128
--- resources/patches/objectool/custom_object_code.asm
+++ resources/patches/objectool/custom_object_code.asm
@@ -1,86 +1,11 @@
 @includefrom objectool.asm
 
-incsrc "../../../shared/freeram.asm"
+; Extended Objects 98-CF reserved by UberASM Object system in the baserom
+incsrc "baserom_custom_objects.asm"
 
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-; code for extended objects 98-FF
+; code for extended objects D0-FF
 ;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-; Extended Objects 98-CF reserved by UberASM Object system
-
-CustExObj98:
-CustExObj99:
-CustExObj9A:
-CustExObj9B:
-CustExObj9C:
-CustExObj9D:
-CustExObj9E:
-CustExObj9F:
-CustExObjA0:
-CustExObjA1:
-CustExObjA2:
-CustExObjA3:
-CustExObjA4:
-CustExObjA5:
-CustExObjA6:
-CustExObjA7:
-CustExObjA8:
-CustExObjA9:
-CustExObjAA:
-CustExObjAB:
-CustExObjAC:
-CustExObjAD:
-CustExObjAE:
-CustExObjAF:
-CustExObjB0:
-CustExObjB1:
-CustExObjB2:
-CustExObjB3:
-CustExObjB4:
-CustExObjB5:
-CustExObjB6:
-CustExObjB7:
-CustExObjB8:
-CustExObjB9:
-CustExObjBA:
-CustExObjBB:
-CustExObjBC:
-CustExObjBD:
-CustExObjBE:
-CustExObjBF:
-CustExObjC0:
-CustExObjC1:
-CustExObjC2:
-CustExObjC3:
-CustExObjC4:
-CustExObjC5:
-CustExObjC6:
-CustExObjC7:
-CustExObjC8:
-CustExObjC9:
-CustExObjCA:
-CustExObjCB:
-CustExObjCC:
-CustExObjCD:
-CustExObjCE:
-CustExObjCF:
-.set_object_flag
-    TXA				; get 2 * object number from X
-    LSR  			; divide by 2 since X was a word index
-	TAY  			; cache in Y
-	LSR #3			; divide by 8 since we have 8 bits/flags per byte
-	TAX				; use as index into the FreeRAM later
-	TYA				; restore object number from Y
-	AND #$07		; modulo 8 since we have 8 bits/flags per byte
-	TAY				; use result as loop variable
-	LDA ..masks,y	; load correct bit mask for corresponding bit
-    ORA !objectool_level_flags_bank,x   ; account for previously set flags
-    STA !objectool_level_flags_bank,x   ; store flag byte
-    RTS
-
-..masks
-	db 1,2,4,8,16,32,64,128
-
 CustExObjD0:
 CustExObjD1:
 CustExObjD2:
